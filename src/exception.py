55f8aafea2b0db99bdc7acb57564831fa00993df
James <rodriguesjames123@gmail.com>
2024-07-09 14:41:52 +0800

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-import logging
+#whenever you want to use logging always have to import from logger
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -8,11 +9,15 @@ def error_message_detail(error, error_detail:sys):
         file_name, exc_tb.tb_lineno,str(error)
     )
     return error_message
-    
+
+#class allows you to handle errors in more structured way    
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
+        #passing the error message to the base Exception class
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail= error_detail)
     
     def __str__(self):
         return self.error_message
+
+
--- src/logger.py
+++ src/logger.py
@@ -6,11 +6,14 @@ LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 log_dir = os.path.join(os.getcwd(),"logs")
 os.makedirs(log_dir,exist_ok=True)
 
+# Define the full log file path
 LOG_FILE_PATH = os.path.join(log_dir,LOG_FILE)
 
+#Always need to configure
 logging.basicConfig(
     filename = LOG_FILE_PATH,
     format = "[ %(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    #only messages with this level or higher will be logged
     level = logging.INFO
 )
 
